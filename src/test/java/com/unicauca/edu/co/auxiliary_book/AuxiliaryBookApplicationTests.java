d5a1f8c4197ea4ce3e1c8f0891ed00d3e0f313f8
AlejFernandezV <alejfernandez@unicauca.edu.co>
2025-11-14 20:25:58 -0500

    Fix: Class tests name

--- src/test/java/com/unicauca/edu/co/auxiliary_book/AuxiliaryBookApplicationTests.java
+++ src/test/java/com/unicauca/edu/co/auxiliary_book/AuxiliaryBookApplicationTests.java
@@ -1,8 +1,7 @@
 package com.unicauca.edu.co.auxiliary_book;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class AuxiliarBookApplicationTests {
+class AuxiliaryBookApplicationTests {
 }
