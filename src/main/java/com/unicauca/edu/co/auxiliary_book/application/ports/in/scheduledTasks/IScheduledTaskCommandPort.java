d714a8c3f523e11a78f1ea8ba7028bb90ddfb42f
AlejFernandezV <alejfernandez@unicauca.edu.co>
2026-02-11 10:48:36 -0500

    Funcionamiento correcto

--- src/main/java/com/unicauca/edu/co/auxiliary_book/application/ports/in/scheduledTasks/IScheduledTaskCommandPort.java
+++
@@ -1,7 +0,0 @@
-package com.unicauca.edu.co.auxiliary_book.application.ports.in.scheduledTasks;
-
-import com.unicauca.edu.co.auxiliary_book.domain.models.scheduledTasks.ScheduledTask;
-
-public interface IScheduledTaskCommandPort {
-    ScheduledTask scheduleTask(ScheduledTask scheduledTask);
-}
